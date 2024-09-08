a10f91086b733985d7eda1f5a9332a2c89a58d8d
devsTudu <65746343+devsTudu@users.noreply.github.com>
2024-09-08 20:02:35 +0530

    utr debug

--- reception/bank.py
+++ reception/bank.py
@@ -2,11 +2,11 @@ import datetime
 from os import getenv
 from dotenv import load_dotenv
 from requests import get
-from .main import UserDatabase
+#from .main import UserDatabase
 
 load_dotenv()
 
-def check_amount_received(utr_no):
+def check_amount_received(utr_no,prin='No'):
     """To check and validate for any recharge, returns False if no amount received"""
     current_datetime = datetime.datetime.now()
     previous_datetime = current_datetime - datetime.timedelta(hours=72)
@@ -15,14 +15,13 @@ def check_amount_received(utr_no):
 
     url = f"https://payments-tesseract.bharatpe.in/api/v1/merchant/transactions?module=PAYMENT_QR&merchantId=34672612&sDate={start_date}&eDate={end_date}"
     headers = {"Token": getenv("BHARATPE_TOKEN")}
-
     response = get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
         transactions = data['data']['transactions']
         for transaction in transactions:
-            # print(utr_no)
-            if transaction['bankReferenceNo'] == utr_no and transaction[
+            utr = int(transaction['bankReferenceNo'])
+            if utr == utr_no and transaction[
                     'status'] == 'SUCCESS':
                 var = transaction['amount']
                 return var
@@ -30,13 +29,13 @@ def check_amount_received(utr_no):
     return False
 
 def reply_for_utr(utr,user_id):
-    amount = check_amount_received(utr)
+    amount = check_amount_received(utr,'y')
     if not amount:
         return "UTR didnot match, please check it again, or enter after few minutes"
     if UserDatabase().record_recharge(user_id=user_id,utr=utr,amount=amount):
-
-        return "Recharge sucessfull"
+       return "Recharge sucessfull"
     else:
         return "UTR already used, sorry can't repeat"
 
-
+if __name__=='__main__':
+    print(reply_for_utr(425281192577,890642031))
